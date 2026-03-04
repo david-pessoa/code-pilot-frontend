d8725c1675cab7276792eed43bef5d7bce5e9336
david-pessoa <pessoa.dvlbp@gmail.com>
2026-03-04 12:30:12 -0300

    finaliza versão para PC da tela de login

--- src/components/Auth/LoginContainer.jsx
+++ src/components/Auth/LoginContainer.jsx
@@ -5,15 +5,25 @@ export default function LoginContainer() {
     <>
       <h2>Bem Vindo de Volta!</h2>
       <form>
-        <label for="email">Email de login:</label>
-        <input type="text" name="email" id="email" />
-        <label for="password">Senha:</label>
-        <input type="password" name="password" id="password" />
-        <a href="#">Esqueci minha senha</a>
-        <a href="/register">
-          <button className={`${styles.button} ${styles.registerBtn}`}>Cadastre-se</button>
-        </a>
-        <button className={`${styles.button} ${styles.loginBtn}`}>Login</button>
+        <fieldset>
+          <div className={styles.inputField}>
+            <label htmlFor="email">Email de login:</label>
+            <input type="text" name="email" id="email" />
+          </div>
+          <div className={styles.inputField}>
+            <label htmlFor="password">Senha:</label>
+            <input type="password" name="password" id="password" />
+          </div>
+        </fieldset>
+        <div className={styles.forgotPassword}>
+          <a href="#">Esqueci minha senha</a>
+        </div>
+        <div className={styles.buttonContainer}>
+          <a href="/register">
+            <button className={`${styles.button} ${styles.registerBtn}`}>Cadastre-se</button>
+          </a>
+          <button className={`${styles.button} ${styles.loginBtn}`}>Login</button>
+        </div>
       </form>
     </>
   );
